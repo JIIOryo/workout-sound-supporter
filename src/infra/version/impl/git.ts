836b4e32e83adf0df542e0d12a3d9330c3ceeb65
JIIOryo <rkawaguchim4w@gmail.com>
2023-01-08 20:24:02 +0900

    Fix bug

--- src/infra/version/impl/git.ts
+++ src/infra/version/impl/git.ts
@@ -13,7 +13,7 @@ export class GitVersion implements infra.version.Interface.IVersion {
 
   public get = async (): Promise<string> => {
     try {
-      const version = childProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+      const version = childProcess.execSync('git describe --tags').toString().trim()
       return version
     } catch (e) {
       this.logger.error('Failed to get version', e)
